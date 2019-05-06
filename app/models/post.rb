47540d24eebf4c5a999c490d10cf4c89ac204bf1
Jesus Santizo <jsantizo@selectrehab.com>
2019-05-06 15:05:24 -0500

    Implemented relationship between post and comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  # dependent: :destroy will delete all the
+  # comments associated witht the post if the
+  # post gets deleted
 end
