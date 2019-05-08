1ff9081754e4c2b5fb6c6f1248cc4eb867cdd2cb
Jesus Santizo <jsantizo@selectrehab.com>
2019-05-08 12:07:13 -0500

    Created nested routes for comments, updated comments controller, implemented comment section on show posts page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
+  resources :posts do
+    # Comments is a nested route to Posts
+    resources :comments
+  end
   root to: 'posts#index'
   get '/my_posts', to: 'posts#my_posts'
 end
